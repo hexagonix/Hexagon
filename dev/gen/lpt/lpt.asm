9ec7ab60ec493c52451d5e5a5b9d78f5f11a5355
Felipe Lunkes <felipenldev@gmail.com>
2023-12-22 11:14:32 -0300

    v1.0.1-beta25

--- dev/gen/lpt/lpt.asm
+++ dev/gen/lpt/lpt.asm
@@ -12,7 +12,7 @@
 ;;
 ;;                          Kernel Hexagon - Hexagon kernel
 ;;
-;;                 Copyright (c) 2015-2023 Felipe Miguel Nery Lunkes
+;;                 Copyright (c) 2015-2024 Felipe Miguel Nery Lunkes
 ;;                Todos os direitos reservados - All rights reserved.
 ;;
 ;;************************************************************************************
@@ -35,7 +35,7 @@
 ;;
 ;; BSD 3-Clause License
 ;;
-;; Copyright (c) 2015-2023, Felipe Miguel Nery Lunkes
+;; Copyright (c) 2015-2024, Felipe Miguel Nery Lunkes
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
